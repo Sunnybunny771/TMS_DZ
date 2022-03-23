8a9b11b97a7965d2f173871c7386daaf07c16d36
Sunnybunny771 <101988418+Sunnybunny771@users.noreply.github.com>
2022-03-23 11:48:49 +0300

    Add files via upload

---
+++ dz4.py
@@ -0,0 +1,13 @@
+# Task 1
+a = [i for i in range(1,20)]
+print(a)
+
+# task 2 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+def list(numbers: list)-> list:
+   numbers_new = [i for i in numbers]
+   return numbers_new
+   
+print(list(numbers))
+
+

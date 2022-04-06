e46cf4be5627a5fba3f2ea26df0299f0294f8cd6
Sunnybunny771 <101988418+Sunnybunny771@users.noreply.github.com>
2022-04-07 00:13:48 +0300

    dz8

---
+++ dz8.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+@dataclass
+class Cars:
+    brand: str
+    model: str
+    horsepower: int
+car_1 = Cars('Audi', 'A3', 243)
+car_2 = Cars('LADA', 'Granta', 98)
+car_3 = Cars('Mazda', 'CX-9', 189)
+
+@staticmethod
+def plus_speeds(x,y,z):
+    return x + y + z
+print(plus_speeds(243,98,189))
+
+@classmethod
+def 

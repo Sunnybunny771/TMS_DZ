0d9973d17bad695dce8c03319b914adf8bd934b9
Sunnybunny771 <101988418+Sunnybunny771@users.noreply.github.com>
2022-04-07 14:30:56 +0300

    Add files via upload

---
+++ dz4.py
@@ -0,0 +1,12 @@
+# Task 1
+
+#a = [i for i in range(1,20)]
+#print(a)
+
+# task 2 
+
+def get_numbers_list(amount: list)-> list:
+   return [i for i in range (amount)]
+print(get_numbers_list(24))
+
+

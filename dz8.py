266679a654a587ac7f5fac62067f9c4359bfcb03
Гребенюк Александр Алексанрович <a.grebenyuk@ka.local>
2022-04-07 16:00:47 +0300

    dz8_not_finished

--- dz8.py
+++ dz8.py
@@ -12,7 +12,10 @@ car_3 = Cars('Mazda', 'CX-9', 189)
 @staticmethod
 def plus_speeds(x,y,z):
     return x + y + z
-print(plus_speeds(243,98,189))
+#print(plus_speeds(243,98,189))
+print(type(car_1))
+#@classmethod
+#def change_horsepower(cls,):
+ #   return cls 
 
-@classmethod
-def 
+#print(Cars.brand())

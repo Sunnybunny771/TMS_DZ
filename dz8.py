24accc0ec0236cd7b7b93966be9fb12363d587ec
Гребенюк Александр Алексанрович <a.grebenyuk@ka.local>
2022-04-11 09:09:42 +0300

    not_finised

--- dz8.py
+++ dz8.py
@@ -1,21 +1,22 @@
-from dataclasses import dataclass
+from datetime import date
+class Person:
+    def __init__(self, name , age):
+         self.name = name
+         self.age = age
 
-@dataclass
-class Cars:
-    brand: str
-    model: str
-    horsepower: int
-car_1 = Cars('Audi', 'A3', 243)
-car_2 = Cars('LADA', 'Granta', 98)
-car_3 = Cars('Mazda', 'CX-9', 189)
+@classmethod
+def birth_year(cls, name, year):
+    return cls(name, date.today().year - year)
+
 
 @staticmethod
-def plus_speeds(x,y,z):
-    return x + y + z
-#print(plus_speeds(243,98,189))
-print(type(car_1))
-#@classmethod
-#def change_horsepower(cls,):
- #   return cls 
+def is_adult(age):
+    return age>18
 
-#print(Cars.brand())
+person1 = Person('Misha', 23)
+person2 = Person('Kate',32)
+
+#print(person1.name, person1.age)
+
+#print(person1.birth_year(2005))
+Person.is_adult(23)

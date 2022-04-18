5912875070112e8a3b8f84153bfb17a12043637d
Гребенюк Александр Алексанрович <a.grebenyuk@ka.local>
2022-04-18 10:02:33 +0300

    done

--- dz10.py
+++ dz10.py
@@ -1,9 +1,9 @@
 # Task сделать генератор геометрической прогрессии 
-def geom_prg(q):
-    for i in range(1,9):
-        yield i * q**(i-1) # формула геом прогрессии
+def geom_prg(number):
+    for i in range(1, number):
+        yield i * 3**(i-1) # формула геом прогрессии
 
-g = geom_prg(3)
+g = geom_prg(8)
 for i in g:
     print(i)
 
